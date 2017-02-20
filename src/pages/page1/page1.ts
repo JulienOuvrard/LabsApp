880711967f3c83db0967fb67796387107f694864
Julien Ouvrard <julien.ouvrard311@gmail.com>
2017-02-20 23:18:48 +0100

    ajout du logout

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AuthService } from '../page0/authService';
+import {Page0} from '../page0/page0';
+
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController) {
-    
+  user:IUser;
+
+  constructor(public navCtrl: NavController,private auth: AuthService) {
+    this.user =  this.auth.getUserInfo();
   }
 
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+        this.navCtrl.setRoot(Page0)
+    });
+  }
 }
