6297b7c7d8831c7607cd1f585eeb2ebfe1c63427
Julien Ouvrard <julien.ouvrard311@gmail.com>
2017-02-22 20:41:20 +0100

    amelioration css + dates

--- src/components/workshop.ts
+++ src/components/workshop.ts
@@ -1,26 +1,27 @@
 import { Rooms } from './rooms'
 import { IWorkshop } from '../interfaces/IWorkshop'
 
-export class Workshop implements IWorkshop{
-    id:Number;
-    nom:String;
-    theme:String;
-    date:Date;
-    description:String;  
-    prerequis:String;
-    salle:Rooms;
-    places:Number;
+export class Workshop implements IWorkshop {
+    id: Number;
+    nom: String;
+    theme: String;
+    date: Date;
+    description: String;
+    prerequis: String;
+    salle: Rooms;
+    places: Number;
 
-    comments:IComment[];
-    participations:IParticipation[];
-    users:IUser[];
+    dateString: String;
+    comments: IComment[];
+    participations: IParticipation[];
+    users: IUser[];
     remainsPlaces: number;
 
-    constructor(id:Number, nom:String, theme:String, date:Date, description:String, prerequis:String, salle:Rooms, places:Number){
+    constructor(id: Number, nom: String, theme: String, date: Date, description: String, prerequis: String, salle: Rooms, places: Number) {
         this.id = id;
         this.nom = nom;
         this.theme = theme;
-        this.date = date;
+        this.date = new Date(date);
         this.description = description;
         this.prerequis = prerequis;
         this.salle = salle;
@@ -29,33 +30,42 @@ export class Workshop implements IWorkshop{
         this.users = [];
         this.comments = [];
         this.participations = [];
+        this.dateString = this.transformDate(date);
     }
 
-    addComment(comment:IComment){
+    transformDate(date:Date) {
+        date = new Date(date);
+        var d =('0' + date.getDate()).slice(-2);
+        var m = ('0' + (date.getMonth()+1)).slice(-2);
+        var y = date.getFullYear();
+        return [d, m, y].join('/');
+    }
+
+    addComment(comment: IComment) {
         this.comments.push(comment);
     }
 
-    addParticipation(participation:IParticipation){
+    addParticipation(participation: IParticipation) {
         this.participations.push(participation);
     }
 
-    addPlace(user:IUser){
-        if(this.remainsPlaces < this.places){
+    addPlace(user: IUser) {
+        if (this.remainsPlaces < this.places) {
             this.remainsPlaces++;
-            if(this.present(user)){
+            if (this.present(user)) {
                 delete this.users[this.users.indexOf(user)];
             }
         }
     }
 
-    removePlace(user:IUser){
-        if(this.remainsPlaces >= this.places){
+    removePlace(user: IUser) {
+        if (this.remainsPlaces >= this.places) {
             this.remainsPlaces--;
             this.users.push(user);
         }
     }
 
-    present(user:IUser){
+    present(user: IUser) {
         return this.users.indexOf(user) !== -1;
     }
 }
